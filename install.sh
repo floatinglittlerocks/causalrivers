9af90eebf538a9ca26a8c8e9708e3bdee74122a2
Gideon-Stein <67389361+Gideon-Stein@users.noreply.github.com>
2025-02-19 17:31:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-conda create -f benchmark_env.yml
+conda env create -f benchmark_env.yml
 wget https://github.com/CausalRivers/benchmark/releases/download/First_release/product.zip
 unzip product
 rm product.zip
